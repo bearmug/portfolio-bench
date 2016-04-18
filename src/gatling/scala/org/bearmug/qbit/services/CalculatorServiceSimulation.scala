191eccb40e542705860fa46a5668cf0fc26056eb
bearmug <pavel.fadeev@gmail.com>
2016-04-18 23:07:23 +0000

    promote more gatling works

--- src/gatling/scala/org/bearmug/qbit/services/CalculatorServiceSimulation.scala
+++ src/gatling/scala/org/bearmug/qbit/services/CalculatorServiceSimulation.scala
@@ -1,35 +1,53 @@
 package org.bearmug.qbit.services
 
-import com.excilys.ebi.gatling.core.Predef._
-import com.excilys.ebi.gatling.http.Predef._
-import com.excilys.ebi.gatling.jdbc.Predef._
-import akka.util.duration._
-import bootstrap._
-import assertions._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import org.bearmug.qbit.PortHelper
+import io.advantageous.qbit.server.EndpointServerBuilder
+import io.advantageous.qbit.server.ServiceEndpointServer
+import org.bearmug.qbit.services.CalculatorService
 
 class CalculatorServiceSimulation extends Simulation {
     
+    var freePort = -1
+    var server = "none"
+    val random = scala.util.Random
+    
     before {
-        println("Simulation is about to start!")
+        println("CalculatorServiceSimulation is about to start!")
+        
+        freePort = PortHelper.availablePort()
+        server = new EndpointServerBuilder().setPort(freePort).build()
+        server.initServices(service)
+        server.start()
     }
 
     after {
-        println("Simulation is finished!")
+        println("CalculatorServiceSimulation is finished!")
+        
+        server.stop()
+        PortHelper.releasePort(freePort)
     }
     
-    val scn = scenario("Calculator usage scenario").repeat(10) {
-        // exec(
-        //     http("Add Numbers")
-        //         .get("http://localhost:${freePort}/services/calc/add/${parameterOne}/${parameterTwo}")
-        //         .check(status.is(200))
-        // )
+    val scn = scenario("Calculator usage scenario").repeat(1) {
+        
+        val parameterOne = random.nextInt(1000)
+        val parameterTwo = random.nextInt(1000)
+        
+        exec(
+            http("Add Numbers")
+                .get("http://localhost:${freePort}/services/calc/add/${parameterOne}/${parameterTwo}")
+                .check(status.is(200))
+        )
     
-        // exec(
-        //     http("Multiply Numbers")
-        //         .get("http://localhost:${freePort}/services/calc/mult/${parameterOne}/${parameterTwo}")
-        //         .check(status.is(200))
-        // )
+        exec(
+            http("Multiply Numbers")
+                .get("http://localhost:${freePort}/services/calc/mult/${parameterOne}/${parameterTwo}")
+                .check(status.is(200))
+        )
   }
 
-//   setUp(scn.users(200).ramp(100))
+  setUp ( 
+        scn.inject(atOnceUsers(1))
+  )
 }
