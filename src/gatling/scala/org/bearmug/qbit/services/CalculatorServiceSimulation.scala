24b800e4736e73be081556d113908cca05601a87
bearmug <pavel.fadeev@gmail.com>
2016-04-19 22:02:31 +0000

    enabl real gatling simulation

--- src/gatling/scala/org/bearmug/qbit/services/CalculatorServiceSimulation.scala
+++ src/gatling/scala/org/bearmug/qbit/services/CalculatorServiceSimulation.scala
@@ -3,46 +3,23 @@ package org.bearmug.qbit.services
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import org.bearmug.qbit.PortHelper
-import io.advantageous.qbit.server.EndpointServerBuilder
-import io.advantageous.qbit.server.ServiceEndpointServer
-import org.bearmug.qbit.services.CalculatorService
 
 class CalculatorServiceSimulation extends Simulation {
     
-    var freePort = -1
-    var server = "none"
     val random = scala.util.Random
-    
-    before {
-        println("CalculatorServiceSimulation is about to start!")
-        
-        freePort = PortHelper.availablePort()
-        server = new EndpointServerBuilder().setPort(freePort).build()
-        server.initServices(service)
-        server.start()
-    }
-
-    after {
-        println("CalculatorServiceSimulation is finished!")
-        
-        server.stop()
-        PortHelper.releasePort(freePort)
-    }
     
     val scn = scenario("Calculator usage scenario").repeat(1) {
         
-        val parameterOne = random.nextInt(1000)
-        val parameterTwo = random.nextInt(1000)
-        
         exec(
+            
             http("Add Numbers")
-                .get("http://localhost:${freePort}/services/calc/add/${parameterOne}/${parameterTwo}")
+                .get("http://localhost:8080/services/calc/add/22/55")
                 .check(status.is(200))
         )
     
         exec(
             http("Multiply Numbers")
-                .get("http://localhost:${freePort}/services/calc/mult/${parameterOne}/${parameterTwo}")
+                .get("http://localhost:8080/services/calc/mult/12/24")
                 .check(status.is(200))
         )
   }
--- src/main/java/org/bearmug/qbit/MainService.java
+++ src/main/java/org/bearmug/qbit/MainService.java
@@ -5,13 +5,24 @@ import io.advantageous.qbit.server.EndpointServerBuilder;
 import io.advantageous.qbit.server.ServiceEndpointServer;
 import org.bearmug.qbit.services.InstrumentService;
 import org.bearmug.qbit.services.PingService;
+import org.bearmug.qbit.services.CalculatorService;
 
 
 public class MainService {
 
+    private ServiceEndpointServer server;
+
     public static void main(String[] args) {
-        ServiceEndpointServer server = new EndpointServerBuilder().setPort(8080).build();
-        server.initServices(new PingService(), new InstrumentService());
+        new MainService().start();
+    }
+    
+    public void start() {
+        server = new EndpointServerBuilder().setPort(8080).build();
+        server.initServices(new PingService(), new CalculatorService());
         server.start();
+    }
+    
+    public void stop() {
+        server.stop();
     }
 }
